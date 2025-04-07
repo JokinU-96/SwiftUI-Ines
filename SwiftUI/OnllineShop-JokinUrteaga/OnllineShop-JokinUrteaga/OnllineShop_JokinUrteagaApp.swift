2e169007d28509724990f4b01d5efd59dc008fbf
JokinU <jokin.urteaga@ikasle.egibide.org>
2025-04-07 18:37:13 +0200

    Cambios

---
+++ SwiftUI/OnllineShop-JokinUrteaga/OnllineShop-JokinUrteaga/OnllineShop_JokinUrteagaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnllineShop_JokinUrteagaApp.swift
+//  OnllineShop-JokinUrteaga
+//
+//  Created by  on 31/3/25.
+//
+
+import SwiftUI
+
+@main
+struct OnllineShop_JokinUrteagaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
